cfb3b887f803a81cf4e4b4c995269a16e781652d
LadaSPB <LadaSpb@yandex.ru>
2022-12-07 19:50:26 +0300

    добавила пробелы между строк

--- T01/Program.cs
+++ T01/Program.cs
@@ -24,6 +24,7 @@ string[] array = new string[massif];
 InputArray(array);
 Console.WriteLine();
 Console.WriteLine($"Введённый первоначальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine();
 Console.ForegroundColor = ConsoleColor.DarkCyan;
 Console.WriteLine("Полученный массив: ");
 NewArray(array);
